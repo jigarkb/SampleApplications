1bc6b067d233b6cadb718e83966a49c7f032c60a
Jigar Bhatt <jigarbhatt93@gmail.com>
2018-04-18 12:46:45 -0700

    Sample Ruby on Rails Movie App

--- Ruby-RubyOnRails/config/routes.rb
+++ Ruby-RubyOnRails/config/routes.rb
@@ -9,5 +9,10 @@ Rails.application.routes.draw do
   get '/destinations/:id' => 'destinations#show', as: :destination
   get '/destinations/:id/edit' => 'destinations#edit', as: :edit_destination
   patch '/destinations/:id' => 'destinations#update'
+
+  get '/movies' => 'movies#index'
+  get '/movies/:id' => 'movies#show', as: :movie
+  get '/actors' => 'actors#index'
+  get '/actors/:id' => 'actors#show', as: :actor
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
